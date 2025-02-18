af480cc4d590cd81d4750400f9ae762513b43b46
Dave Farinelli <d@dfar.io>
2025-02-18 15:59:03 +0000

    one more tweak

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,7 +1,6 @@
 rm -rf ./node_modules
 
 npm i
-# need to commit this file first
-#git update-index --skip-worktree ./src/environments/version.ts
+git update-index --skip-worktree ./src/environments/version.ts
 
 npm i -g source-map-explorer
