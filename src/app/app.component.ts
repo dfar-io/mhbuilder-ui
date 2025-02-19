2e6ca24ed3966da3ac7caf2932f553ff932d6aee
Dave Farinelli <d@dfar.io>
2025-02-19 01:09:43 +0000

    Progress!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,26 +44,67 @@ export class AppComponent implements OnInit {
 
   calc() {
     const startTime = performance.now(); // Start timing
+    // 0 - low rank
+    // 1 - high rank
+    // 2 - master rank
+    const rarity = 0;
+    const skills = ['Attack Boost', 'Weakness Exploit'];
 
-    // this.helmData.forEach(h => {
-    //   this.mailData.forEach(m => {
-    //     this.braceData.forEach(b => {
-    //       this.coilData.forEach(c => {
-    //         this.greaveData.forEach(g => {
-    //           // const def = h.baseDefense +
-    //           //             m.baseDefense +
-    //           //             b.baseDefense +
-    //           //             c.baseDefense +
-    //           //             g.baseDefense;
-    //         });
-    //       });
-    //     });
-    //   });
-    // });
+    const eligibleHelms = this.helmData.filter(h =>
+      h.rank <= rarity &&
+      h.skills.some(s => skills.indexOf(s.name) > -1)
+    );
+    const eligibleMails = this.mailData.filter(h =>
+      h.rank <= rarity &&
+      h.skills.some(s => skills.indexOf(s.name) > -1)
+    );
+    const eligibleBraces = this.braceData.filter(h =>
+      h.rank <= rarity &&
+      h.skills.some(s => skills.indexOf(s.name) > -1)
+    );
+    const eligibleCoils = this.coilData.filter(h =>
+      h.rank <= rarity &&
+      h.skills.some(s => skills.indexOf(s.name) > -1)
+    );
+    const eligibleGreaves = this.greaveData.filter(h =>
+      h.rank <= rarity &&
+      h.skills.some(s => skills.indexOf(s.name) > -1)
+    );
+
+    const eligibleHelmCount = eligibleHelms.length;
+    const eligibleMailCount = eligibleMails.length;
+    const eligibleBraceCount = eligibleBraces.length;
+    const eligibleCoilCount = eligibleCoils.length;
+    const eligibleGreaveCount = eligibleGreaves.length;
+
+    console.log(`helms: ${eligibleHelms.length}`);
+    console.log(`mails: ${eligibleMails.length}`);
+    console.log(`braces: ${eligibleBraces.length}`);
+    console.log(`coils: ${eligibleCoils.length}`);
+    console.log(`greaves: ${eligibleGreaves.length}`);
+
+    for(let i=0; i < eligibleHelmCount; i++) {
+      for(let j=0; j < eligibleMailCount; j++) {
+        for(let k=0; k < eligibleBraceCount; k++) {
+          for(let l=0; l < eligibleCoilCount; l++) {
+            for(let n=0; n < eligibleGreaveCount; n++) {
+              const h = eligibleHelms[i];
+              const m = eligibleMails[j]
+              const b = eligibleBraces[k];
+              const c = eligibleCoils[l];
+              const g = eligibleGreaves[n];
+
+              const def = h.baseDefense + m.baseDefense + b.baseDefense + c.baseDefense + g.baseDefense;
+
+              console.log(`${def}: ${h.name}, ${m.name}, ${b.name}, ${c.name}, ${g.name}`);
+            }
+          }
+        }
+      }
+    }
 
     const endTime = performance.now(); // End timing
     const duration = endTime - startTime; // Calculate duration
-
     console.log(`Method took ${duration} milliseconds to run.`);
   }
 }
